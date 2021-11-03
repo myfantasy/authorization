9ea7212870913244c46daaf8fc526e11a2be5329
Ilya Shcherbina <sch@myfantasy.ru>
2021-11-03 21:27:29 +0300

    Action and ObjectType for impersonate

---
+++ consts.go
@@ -0,0 +1,6 @@
+package authorization
+
+import ajt "github.com/myfantasy/api_json_types"
+
+const ObjectTypeAuthorization ajt.ObjectType = "authorization"
+const ImpersonateAction ajt.Action = "impersonate"
